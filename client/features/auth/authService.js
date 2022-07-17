9b08d8a3d9f08d5fd114dc3a296c4acba376db98
MagistrUMDev <97807772+MagistrUMDev@users.noreply.github.com>
2022-07-17 23:44:48 +0300

    Update authService.js

--- client/features/auth/authService.js
+++ client/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `http://localhost:5000/api/users/`;
+const API_URL = `https://vlasovgenpass.herokuapp.com/api/users/`;
 
 // Register User
 const register = async (userData) => {
