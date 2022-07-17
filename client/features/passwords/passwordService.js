197efa7ff3cda9b4ad02daa4aab3987cb880a850
MagistrUMDev <97807772+MagistrUMDev@users.noreply.github.com>
2022-07-17 23:48:19 +0300

    Update passwordService.js

--- client/features/passwords/passwordService.js
+++ client/features/passwords/passwordService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `https://vlasovgenpass.herokuapp.com/`;
+const API_URL = `https://vlasovgenpass.herokuapp.com/api/savedpasswords/`;
 
 // Create new password
 const createPassword = async (passwordData, token) => {
