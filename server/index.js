44acb2cd626830a497ec4d7db2920091f71277a2
MagistrUMDev <97807772+MagistrUMDev@users.noreply.github.com>
2022-07-17 23:34:23 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-app.use(favicon(__dirname + './favicon.png'))
+app.use(favicon(__dirname + './dist/assets/favicon.aeb56eef.ico'))
 
 const userRouter = require("./routes/userRoutes");
 const passwordRouter = require("./routes/savedPasswordRoutes");
