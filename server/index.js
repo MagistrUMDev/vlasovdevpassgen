1c3a25c3bb7b56b4d32f509ad9947245b0d3416c
MagistrUMDev <97807772+MagistrUMDev@users.noreply.github.com>
2022-07-17 23:21:27 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(passwordRouter);
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + '/client'));
+  app.use(express.static(path.join(__dirname, '../client/build')))
   app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "client", "build", "index.html")
